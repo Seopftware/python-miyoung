4538247f46e796f4faae6b46618567bb0183eac9
inseop <inseop0813@gmail.com>
2022-10-24 15:32:23 +0900

    파이썬 자료형 데이터
    (1) 숫자형
    (2) 문자형
    (3) 리스트형
    (4) 딕셔너리형

---
+++ python_data_type.py
@@ -0,0 +1,56 @@
+x = "python" # 문자형
+x = 123
+x = "123"
+x = [1, 2, 3, 4]
+x = {"name":"leo", "age":"32"}
+x = True # False
+# print(type(x))
+
+a = 1 + 1
+a = 100 - 1
+a = 12 * 12
+a = 3 / 4
+# print(a)
+
+a = "Act as thouth \nit is impossible to fail"
+# print(a)
+
+a = '''Act as thouth
+it is impossible to fail'''
+# print(a)
+
+a = '“Failure is simply the opportunity to begin again.” he says.”'
+a = """ "Failure is simply the opportunity to begin again.” he says." """
+a = "\"Failure is simply the opportunity to begin again.\" he says.\""
+# print(a)
+
+a = "A"
+b = "B"
+# print(a*b)
+# print(a*100)
+
+c = "hello pyhton!"
+# print(len(c))
+# print(c[0]) # indexing
+# print(c[0:4]) # slicing, 0 <= c < 5
+# print(c[6:])
+# print(c[:])
+
+x = "TitanicJames"
+title = x[:7]
+director = x[7:]
+# print(title,director)
+
+# a = ["a", "b", "c", "d", "e"]
+a = ["a", "b", ["c", "d", "e"]]
+# print(a[1])
+# print(a[2][1])
+
+t = ("a", "a", "a", "b")
+# print(type(t))
+
+x = {"key1":"value1", "key2":"value2"}
+# print(x["key2"])
+
+x = set(["1","1","1","2"])
+# print(x)
