44db00dafbd8f90ae023d2ce87be67f3ce241c07
inseop <inseop0813@gmail.com>
2022-10-27 16:51:29 +0900

    3day

--- slack_boy.py
+++ slack_boy.py
@@ -2,19 +2,20 @@ import requests
 import json
 
 def send_slack_message():
-    slack_url = "https://hooks.slack.com/services/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"
-    message = """메세지 전송""" 
+    slack_url = "https://hooks.slack.com/services/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"
+    message = """
+    여러분 화이팅!
+    """ 
 
     payloads = {
         "text": message,
-        "username": "수업 신청",
-        "icon_emoji": ":clap:"
     }
 
     response = requests.post(
         slack_url, data=json.dumps(payloads),
         headers={'Content-Type': 'application/json'}
     )
-
-    if response.status_code != 200:
-        print("error" + response.status_code)
+    
+    print(response)
+        
+send_slack_message()
